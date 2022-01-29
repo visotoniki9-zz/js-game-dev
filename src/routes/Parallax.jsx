8e3b4db1f4bbf5ceeda2bfd1a47dbc71898ab4dc
visotoniki9 <d.simon.ssx0@gmail.com>
2022-01-29 18:34:01 +0100

    Basic empty canvas

--- src/routes/Parallax.jsx
+++ src/routes/Parallax.jsx
@@ -1,5 +1,33 @@
+import { useRef, useEffect } from 'react';
+
 function Parallax() {
-  return <div>parallax</div>;
+  const canvasRef = useRef(null);
+  useEffect(() => {
+    // Canvas Setup
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext('2d');
+    canvas.width = 800;
+    canvas.height = 700;
+
+    let gameFrame = 0;
+    let animationFrameId;
+
+    // Animation
+    const render = () => {
+      gameFrame += 1;
+      animationFrameId = window.requestAnimationFrame(render);
+    };
+    render();
+    return () => {
+      window.cancelAnimationFrame(animationFrameId);
+    };
+  });
+  return (
+    <canvas
+      className="border-8 my-4 absolute left-1/2 -translate-x-1/2"
+      ref={canvasRef}
+    />
+  );
 }
 
 export default Parallax;
