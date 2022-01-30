78bab9eff14544ba99c98abce93ccace2d1ba250
visotoniki9 <d.simon.ssx0@gmail.com>
2022-01-30 19:55:14 +0100

    Optimizations

--- src/routes/SpriteAnimations.jsx
+++ src/routes/SpriteAnimations.jsx
@@ -1,41 +1,63 @@
 import { useState, useRef, useEffect } from 'react';
 import shadowDogImage from '../../assets/shadow_dog.webp';
 
+const animationStates = [
+  { name: 'idle', frames: 7 },
+  { name: 'jump', frames: 7 },
+  { name: 'fall', frames: 7 },
+  { name: 'run', frames: 9 },
+  { name: 'dizzy', frames: 11 },
+  { name: 'sit', frames: 5 },
+  { name: 'roll', frames: 7 },
+  { name: 'bite', frames: 7 },
+  { name: 'ko', frames: 12 },
+  { name: 'getHit', frames: 4 },
+];
+
+let gameFrame = 0;
+let animationFrameId;
+const playerImage = new Image();
+playerImage.src = shadowDogImage;
+const spriteWidth = 575;
+const spriteHeight = 523;
+
+const staggerFrames = 5;
+const spriteAnimations = [];
+
+animationStates.forEach((state, index) => {
+  const frames = {
+    loc: [],
+  };
+  for (let j = 0; j < state.frames; j++) {
+    const positionX = j * spriteWidth;
+    const positionY = index * spriteHeight;
+    frames.loc.push({ x: positionX, y: positionY });
+  }
+  spriteAnimations[state.name] = frames;
+});
+
+function draw(ctx, canvas, playerState) {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  const position = Math.floor(gameFrame / staggerFrames)
+    % spriteAnimations[playerState].loc.length;
+  const frameX = spriteWidth * position;
+  const frameY = spriteAnimations[playerState].loc[position].y;
+  ctx.drawImage(
+    playerImage,
+    frameX,
+    frameY,
+    spriteWidth,
+    spriteHeight,
+    0,
+    0,
+    canvas.width,
+    canvas.height,
+  );
+}
+
 function SpriteAnimations() {
-  const [playerState, setPlayerState] = useState('idle');
   const canvasRef = useRef(null);
-
-  const playerImage = new Image();
-  playerImage.src = shadowDogImage;
-  const spriteWidth = 575;
-  const spriteHeight = 523;
-
-  const staggerFrames = 5;
-  const spriteAnimations = [];
-  const animationStates = [
-    { name: 'idle', frames: 7 },
-    { name: 'jump', frames: 7 },
-    { name: 'fall', frames: 7 },
-    { name: 'run', frames: 9 },
-    { name: 'dizzy', frames: 11 },
-    { name: 'sit', frames: 5 },
-    { name: 'roll', frames: 7 },
-    { name: 'bite', frames: 7 },
-    { name: 'ko', frames: 12 },
-    { name: 'getHit', frames: 4 },
-  ];
-
-  animationStates.forEach((state, index) => {
-    const frames = {
-      loc: [],
-    };
-    for (let j = 0; j < state.frames; j++) {
-      const positionX = j * spriteWidth;
-      const positionY = index * spriteHeight;
-      frames.loc.push({ x: positionX, y: positionY });
-    }
-    spriteAnimations[state.name] = frames;
-  });
+  const [playerState, setPlayerState] = useState('idle');
   useEffect(() => {
     // Canvas Setup
     const canvas = canvasRef.current;
@@ -43,35 +65,17 @@ function SpriteAnimations() {
     canvas.width = 600;
     canvas.height = 600;
 
-    let gameFrame = 0;
-    let animationFrameId;
-
     // Animation
     const render = () => {
       gameFrame++;
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      const position = Math.floor(gameFrame / staggerFrames)
-    % spriteAnimations[playerState].loc.length;
-      const frameX = spriteWidth * position;
-      const frameY = spriteAnimations[playerState].loc[position].y;
-      ctx.drawImage(
-        playerImage,
-        frameX,
-        frameY,
-        spriteWidth,
-        spriteHeight,
-        0,
-        0,
-        canvas.width,
-        canvas.height,
-      );
+      draw(ctx, canvas, playerState);
       animationFrameId = window.requestAnimationFrame(render);
     };
     render();
     return () => {
       window.cancelAnimationFrame(animationFrameId);
     };
-  }, []);
+  });
   return (
     <div className="flex flex-col items-center">
       <div className="my-4">
